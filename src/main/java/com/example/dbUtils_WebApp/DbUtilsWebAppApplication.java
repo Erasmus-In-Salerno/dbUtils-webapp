9651a9462547a8e5487ce66535dc4bb1719bea05
awrockla <awrockla@uni-muenster.de>
2024-10-29 15:34:16 +0100

    add basic spring config

---
+++ src/main/java/com/example/dbUtils_WebApp/DbUtilsWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbUtils_WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbUtilsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbUtilsWebAppApplication.class, args);
+	}
+
+}
